a7b5f79d2d25fe514e1b27f24abbf2e9f284ca34
ClaraRuizR <clara.ruiz.ruiz96@gmail.com>
2022-10-21 17:08:14 +0200

    codigo modularizado

--- memorin/js/memorin.js
+++ memorin/js/memorin.js
@@ -1,20 +1,27 @@
-let filas = prompt('¿Cuántas filas quieres?');
-let columnas = prompt('¿Cuántas columnas quieres?');
+iniciarJuego();
 
-if (filas < 2 || columnas < 2 || filas > 25 || columnas > 25 || ((filas * columnas) % 2) != 0){
-    let inputCorrecto = false;
-    while(inputCorrecto == false){
-        window.alert('Error: número incorrecto de celdas. Recuerda que el número de filas y columnas debe ser entre 2 y 25. Además las celdas deben dar un número par.')
-        filas = prompt('¿Cuántas filas quieres?');
-        columnas = prompt('¿Cuántas columnas quieres?');
-        if(filas > 2 && columnas > 2 && filas < 25 && columnas < 25&& ((filas * columnas) % 2) == 0){
-            inputCorrecto == true;
-            break;
+
+function iniciarJuego(){
+    let filas = prompt('¿Cuántas filas quieres?');
+    let columnas = prompt('¿Cuántas columnas quieres?');
+
+    if (filas < 2 || columnas < 2 || filas > 25 || columnas > 25 || ((filas * columnas) % 2) != 0){
+        let inputCorrecto = false;
+        while(inputCorrecto == false){
+            window.alert('Error: debes introducir dos números entre 2 y 25. Además el número total de celdas debe ser un número par.')
+            filas = prompt('¿Cuántas filas quieres?');
+            columnas = prompt('¿Cuántas columnas quieres?');
+            if(filas >= 2 && columnas >= 2 && filas < 25 && columnas < 25&& ((filas * columnas) % 2) == 0){
+                inputCorrecto == true;
+                break;
+            }
         }
     }
+
+    arrayTablero = crearArrayTablero(filas, columnas);
+    pintarTablero(filas, columnas, arrayTablero);
 }
 
-pintarTablero(filas, columnas);
 
 function pintarTablero (filas, columnas){
     document.write('<table>');
@@ -23,12 +30,26 @@ function pintarTablero (filas, columnas){
             document.write('<tr>');
         
             for (let j = 0; j < columnas; j++) {
-                document.write(`<td><p id="celdas"></p></td>`);
+                document.write(`<td><p id="celdas">${arrayTablero[i][j]}</p></td>`);
             }
         
             document.write('</tr>');
         }
         document.write('</table>');
+}
+
+function crearArrayTablero(filas, columnas){
+    let arrayTablero = [];
+
+    for (let i = 0; i < filas; i++){
+        arrayTablero[i] = new Array(columnas);
+
+        for (let j= 0; j < columnas; j++){
+            arrayTablero[i][j] = 'a';
+        }
+    }
+
+    return arrayTablero
 }
 
 

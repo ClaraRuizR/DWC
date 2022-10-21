6b7f49b1786230fb04512860c47b8ecc9e73e344
ClaraRuizR <clara.ruiz.ruiz96@gmail.com>
2022-10-21 16:48:49 +0200

    pintar tablero con límites

--- memorin/js/memorin.js
+++ memorin/js/memorin.js
@@ -1,6 +1,18 @@
 let filas = prompt('¿Cuántas filas quieres?');
 let columnas = prompt('¿Cuántas columnas quieres?');
 
+if (filas < 2 || columnas < 2 || filas > 25 || columnas > 25 || ((filas * columnas) % 2) != 0){
+    let inputCorrecto = false;
+    while(inputCorrecto == false){
+        window.alert('Error: número incorrecto de celdas. Recuerda que el número de filas y columnas debe ser entre 2 y 25. Además las celdas deben dar un número par.')
+        filas = prompt('¿Cuántas filas quieres?');
+        columnas = prompt('¿Cuántas columnas quieres?');
+        if(filas > 2 && columnas > 2 && filas < 25 && columnas < 25&& ((filas * columnas) % 2) == 0){
+            inputCorrecto == true;
+            break;
+        }
+    }
+}
 
 pintarTablero(filas, columnas);
 
